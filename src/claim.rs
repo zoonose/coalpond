c5421c31976e997e54bd63e8836baf89de67554a
failediea <130617343+failediea@users.noreply.github.com>
2024-08-21 12:54:10 -0400

    Update claim.rs

--- src/claim.rs
+++ src/claim.rs
@@ -11,7 +11,7 @@ pub struct ClaimArgs {
         long,
         value_name = "AMOUNT",
         default_value = "0.00",
-        help = "Amount of ore to claim."
+        help = "Amount of coal to claim."
     )]
     pub amount: f64,
 }
@@ -33,7 +33,7 @@ pub fn ask_confirm(question: &str) -> bool {
 }
 
 pub async fn claim(args: ClaimArgs, key: Keypair, url: String, unsecure: bool) {
-    let mut claim_amount = (args.amount * 10f64.powf(ore_api::consts::TOKEN_DECIMALS as f64)) as u64;
+    let mut claim_amount = (args.amount * 10f64.powf(coal_api::consts::TOKEN_DECIMALS as f64)) as u64;
 
     let base_url = url;
     let client = reqwest::Client::new();
@@ -46,22 +46,22 @@ pub async fn claim(args: ClaimArgs, key: Keypair, url: String, unsecure: bool) {
 
     loop {
         let balance = client.get(format!("{}://{}/miner/rewards?pubkey={}", url_prefix, base_url, key.pubkey().to_string())).send().await.unwrap().text().await.unwrap();
-        let balance_grains = (balance.parse::<f64>().unwrap() * 10f64.powf(ore_api::consts::TOKEN_DECIMALS as f64)) as u64;
-        println!("Claimable Rewards: {} ORE", balance);
+        let balance_grains = (balance.parse::<f64>().unwrap() * 10f64.powf(coal_api::consts::TOKEN_DECIMALS as f64)) as u64;
+        println!("Claimable Rewards: {} COAL", balance);
 
         if claim_amount == 0 {
             claim_amount = balance_grains;
         }
 
         if claim_amount > balance_grains {
-            println!("You do not have enough rewards to claim {} ORE.", amount_to_ui_amount(claim_amount, ore_api::consts::TOKEN_DECIMALS));
-            println!("Please enter an amount less than or equal to {} ORE.", amount_to_ui_amount(balance_grains, ore_api::consts::TOKEN_DECIMALS));
+            println!("You do not have enough rewards to claim {} COAL.", amount_to_ui_amount(claim_amount, coal_api::consts::TOKEN_DECIMALS));
+            println!("Please enter an amount less than or equal to {} COAL.", amount_to_ui_amount(balance_grains, coal_api::consts::TOKEN_DECIMALS));
             claim_amount = loop {
                 let mut input = String::new();
                 io::stdout().flush().unwrap();
                 let _ = std::io::stdin().read_line(&mut input);
                 if let Ok(new_amount) = input.trim().parse::<f64>() {
-                    let new_claim_amount = (new_amount * 10f64.powf(ore_api::consts::TOKEN_DECIMALS as f64)) as u64;
+                    let new_claim_amount = (new_amount * 10f64.powf(coal_api::consts::TOKEN_DECIMALS as f64)) as u64;
                     if new_claim_amount <= balance_grains {
                         break new_claim_amount;
                     }
@@ -75,8 +75,8 @@ pub async fn claim(args: ClaimArgs, key: Keypair, url: String, unsecure: bool) {
             format!(
                 "\nYou are about to claim {}.\nAre you sure you want to continue? [Y/n]",
                 format!(
-                    "{} ORE",
-                    amount_to_ui_amount(claim_amount, ore_api::consts::TOKEN_DECIMALS)
+                    "{} COAL",
+                    amount_to_ui_amount(claim_amount, coal_api::consts::TOKEN_DECIMALS)
                 )
             )
             .as_str(),
